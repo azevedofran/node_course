0474919edbe00f1ff73a28334b382933add5bd5d
azevedofran <fjazevedo.11@est.ucab.edu.ve>
2019-05-24 12:19:22 -0400

    Hola mundo html asincrono

---
+++ hola_html.js
@@ -0,0 +1,10 @@
+var http = require("http"),
+    fs = require("fs");
+
+fs.readFile("./index.html",function(err,html){
+  http.createServer(function(req,res){
+    res.write(html);
+    res.end();
+  }).listen(8080);  
+});
+ 
