4773a2fe75ed6b1b78fbcb73b2e559ee2b8c766f
azevedofran <fjazevedo.11@est.ucab.edu.ve>
2019-05-28 11:11:24 -0400

    Render_vars utilizando {} para pasar una variable al html

--- hola_html.js
+++ hola_html.js
@@ -8,3 +8,13 @@ fs.readFile("./index.html",function(err,html){
   }).listen(8080);  
 });
  
+/*var http = require("http"),
+    fs = require("fs");
+
+http.createServer(function(req,res){
+  fs.readFile("./index.html",function(err,html){
+    res.writeHead(200,{"Content-Type":"application/json"});
+    res.write(JSON.stringify({nombre:"Francisco", username:"francisco"}));
+    res.end();
+  });
+}).listen(8080);*/
---
+++ render_vars.js
@@ -0,0 +1,52 @@
+//script completo
+var http = require("http"),
+fs = require("fs");
+
+http.createServer(function(req,res){
+fs.readFile("./index.html",function(err,html){
+var html_string = html.toString();
+// Expresion regular que busca en el codigo donde haya {x}
+var variables = html_string.match(/(?<=\{)(.+)(?=\})/g);
+
+var nombre="Francisco";
+// variable ['nombre']
+for (var i = variables.length - 1; i >= 0; i--) {
+  //Lo ejecutamos como codigo de javaScript
+  //Para obtener el valor de dicha variable
+  var value = eval(variables[i]);
+  //Reemplazar el contenido con llaves {x} por su valor correspondiente
+  html_string = html_string.replace("{"+variables[i]+"}",value);
+
+};
+
+//Mandamos el contenido
+res.writeHead(200,{"Content-Type":"text/html"})
+res.write(html_string);
+res.end();
+});
+}).listen(8080);
+
+/*var http = require("http"),
+    fs = require("fs");
+
+http.createServer(function(req,res){
+
+  fs.readFile("./index.html",function(err,html){
+    var html_string = html.toString();
+
+    var variables = html_string.match(/[^\{\}]+(?=\})/g);
+    var nombre = "Francisco";
+
+    // variable ['nombre']
+    for (var i = variables.lenght - 1; i>= 0; i--){
+      var value = eval(variables[i]);
+      html_string = html_string.replace("{"+variables[i]+"}",value);
+    };
+
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.write(html_string);
+    res.end();
+
+  });
+
+}).listen(8080); */ 
